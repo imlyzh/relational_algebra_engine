a03d01ad47dbaf0ff30759af6155d79c611dcc7e
imlyzh <enterindex@gmail.com>
2021-09-15 13:36:11 +0800

    add copyright to all code file

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,3 +1,19 @@
+/*
+   Copyright (C) 2021-2021 imlyzh.
+
+This file is part of RAE(Relational Algebra Engine).
+RAE is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+RAE is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+You should have received a copy of the GNU General Public License
+along with RAE; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
 pub mod ast;
 pub mod plan;
 pub mod plan_group;
--- src/structs/plan.rs
+++ src/structs/plan.rs
@@ -1,5 +1,21 @@
-use super::{Expr, Symbol};
+/*
+   Copyright (C) 2021-2021 imlyzh.
 
+This file is part of RAE(Relational Algebra Engine).
+RAE is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+RAE is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+You should have received a copy of the GNU General Public License
+along with RAE; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
+use super::{Expr, Symbol};
 
 
 #[derive(Debug, Clone, PartialEq)]
